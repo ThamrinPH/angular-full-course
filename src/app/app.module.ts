df0553718c1fa126448eda12067f25e20229a76d
ThamrinPH <husni.sosnet@gmail.com>
2022-07-26 16:29:43 +0700

    register and call server component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+// import { HttpClient } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
 import { ServerComponent } from './server/server.component';
@@ -10,7 +12,9 @@ import { ServerComponent } from './server/server.component';
     ServerComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    // HttpClient
+    FormsModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
