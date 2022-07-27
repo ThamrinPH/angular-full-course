33517ceca7fb17948c303ad162eaf4d9a995dfd8
ThamrinPH <husni.sosnet@gmail.com>
2022-07-27 11:05:29 +0700

    init files

--- karma.conf.js
+++ karma.conf.js
@@ -25,7 +25,7 @@ module.exports = function (config) {
       suppressAll: true // removes the duplicated traces
     },
     coverageReporter: {
-      dir: require('path').join(__dirname, './coverage/angular-full-course'),
+      dir: require('path').join(__dirname, './coverage/section-3'),
       subdir: '.',
       reporters: [
         { type: 'html' },
