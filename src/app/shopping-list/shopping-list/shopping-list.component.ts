6f16bf1d5bfdc74020c619a30b3a767a1160b1e5
ThamrinPH <husni.sosnet@gmail.com>
2022-07-27 17:13:50 +0700

    58. Creating an Ingredient model

--- src/app/shopping-list/shopping-list/shopping-list.component.ts
+++ src/app/shopping-list/shopping-list/shopping-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ingredient } from 'src/app/shared/ingredient.mode';
 
 @Component({
   selector: 'app-shopping-list',
@@ -7,8 +8,8 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ShoppingListComponent implements OnInit {
 
-  ingredients = [];
-  
+  ingredients: ingredient[] = [];
+
   constructor() { }
 
   ngOnInit(): void {
