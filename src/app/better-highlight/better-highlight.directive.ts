0874b7725564e0cf433161871394ec5aa83b12cc
ThamrinPH <husni.sosnet@gmail.com>
2022-08-01 11:14:22 +0700

    97. Using HostListener to Listen to Host Events

--- src/app/better-highlight/better-highlight.directive.ts
+++ src/app/better-highlight/better-highlight.directive.ts
@@ -1,9 +1,10 @@
-import { Directive, ElementRef, OnInit, Renderer2 } from '@angular/core';
+import { Directive, ElementRef, HostListener, OnInit, Renderer2 } from '@angular/core';
 
 @Directive({
   selector: '[appBetterHighlight]'
 })
 export class BetterHighlightDirective implements OnInit{
+  
 
   constructor(private elementRef: ElementRef, private renderer: Renderer2) { }
 
@@ -11,4 +12,7 @@ export class BetterHighlightDirective implements OnInit{
     this.renderer.setStyle(this.elementRef.nativeElement, 'background-color', 'purple');
   }
 
+  @HostListener('mouseenter') mouseOver() {
+    this.renderer.setStyle(this.elementRef.nativeElement, 'background-color', 'transparent');
+  }
 }
