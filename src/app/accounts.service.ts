724847478a2960e0a969237ac6e942b58899bbf9
ThamrinPH <husni.sosnet@gmail.com>
2022-08-01 15:30:10 +0700

    109. Creating a Data Service

---
+++ src/app/accounts.service.ts
@@ -0,0 +1,36 @@
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AccountsService {
+
+  accounts = [
+    {
+      name: 'Master Account',
+      status: 'active'
+    },
+    {
+      name: 'Testaccount',
+      status: 'inactive'
+    },
+    {
+      name: 'Hidden Account',
+      status: 'unknown'
+    }
+  ];
+
+  addAccount(name: string, status: string) {
+    this.accounts.push({name, status});
+  }
+
+  updateAccount(id: number, status: string) {
+    this.accounts[id].status = status;
+  }
+
+  getAccounts() {
+    return this.accounts;
+  }
+
+  constructor() { }
+}
