92bc399a7f84f63ad9bc750f7e25b42b83534f40
husnisoftbridge <102209718+husnisoftbridge@users.noreply.github.com>
2022-08-18 21:42:39 +0700

    157. Adding Navigation to the App

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,14 +1,9 @@
-import { Component, EventEmitter, Output } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html'
 })
 export class HeaderComponent {
-  @Output() switchHeader = new EventEmitter<string>();
-
-  selectMenu(menu: string)
-  {
-    this.switchHeader.emit(menu)
-  }
+  
 }
