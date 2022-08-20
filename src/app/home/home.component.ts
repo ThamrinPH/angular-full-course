8efcf60e23dcc7d92d04df28b951420e0ce74894
husnisoftbridge <102209718+husnisoftbridge@users.noreply.github.com>
2022-08-20 09:07:04 +0700

    177. Understanding Operators

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnDestroy, OnInit } from '@angular/core';
 import { interval, Subscription, Observable } from 'rxjs';
+import { map, filter } from 'rxjs/operators'
 
 @Component({
   selector: 'app-home',
@@ -36,7 +37,14 @@ export class HomeComponent implements OnInit, OnDestroy {
       }, 1000)
     });
 
-    this.subscription = costumIntervalObservable.subscribe( data => {
+
+    this.subscription = costumIntervalObservable.pipe(
+      filter( data => {
+          return data > 0;
+      }),
+      map( (data:number) => {
+        return 'Round : ' + (++data);
+      })).subscribe( data => {
       console.log(data);
     }, error => {
       alert(error.message);
