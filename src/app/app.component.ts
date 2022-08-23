737c8906bdd160768270386b3e104368d8c88b17
husnisoftbridge <102209718+husnisoftbridge@users.noreply.github.com>
2022-08-23 21:15:31 +0700

    178. Subjects

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,27 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { Subscription } from 'rxjs';
+import { UserService } from './user.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent implements OnInit {
-  constructor() {}
+export class AppComponent implements OnInit, OnDestroy {
+  userActivated = false;
+  private activateSub: Subscription;
 
-  ngOnInit() {}
+  constructor(private userService: UserService) {}
+
+  ngOnInit() {
+    this.activateSub = this.userService.activatedEmitter.subscribe(
+      (didActivated) => {
+        this.userActivated = didActivated;
+      }
+    );
+  }
+
+  ngOnDestroy(): void {
+    this.activateSub.unsubscribe();
+  }
 }
