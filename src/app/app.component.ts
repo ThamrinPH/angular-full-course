13d29e4c2ec46597a9d47ef7d5f63d6793ea4934
husnisoftbridge <102209718+husnisoftbridge@users.noreply.github.com>
2022-08-23 23:16:52 +0700

    186. An Example Form

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,9 +6,7 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  menu:string = 'recipe';
-  
-  onNavigate(menu: string){
-    this.menu = menu;
+  suggestUserName() {
+    const suggestedName = 'Superuser';
   }
 }
