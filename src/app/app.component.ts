d2ab2c9fa2df11e120f5c51d3e3846c9dba223dc
Husni <service.has.solution@gmail.com>
2022-09-08 12:08:05 +0700

    259. Using Types with the HttpClient

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { map } from 'rxjs/operators'
+
+import { Post } from './post.model';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -19,7 +21,7 @@ export class AppComponent implements OnInit {
   onCreatePost(postData: { title: string; content: string }) {
     // Send Http request
     this.http
-    .post(
+    .post<{ name: string } >(
         this.url+'posts.json', 
         postData
       ).subscribe( responseData => {
@@ -32,9 +34,11 @@ export class AppComponent implements OnInit {
   onFetchPosts() {
     // Send Http request
     this.http
-      .get(this.url+'posts.json')
-      .pipe(map( responseData => {
-        const postsArray = [];
+      .get<{ [key: string]: Post } >(this.url+'posts.json')
+      .pipe(
+        // map( (responseData: { [key: string]: Post } ) => {
+        map( (responseData) => {
+        const postsArray: Post[] = [];
 
         for (const key in responseData) {
           if(responseData.hasOwnProperty(key)) {
