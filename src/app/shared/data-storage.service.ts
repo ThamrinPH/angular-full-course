1c11f252eec0523d8517cb27997b8b25cc527c67
Husni <service.has.solution@gmail.com>
2022-09-09 08:26:59 +0700

    282. Storing Recipes

--- src/app/shared/data-storage.service.ts
+++ src/app/shared/data-storage.service.ts
@@ -1,10 +1,29 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 
+import { RecipeService } from '../recipes/recipe.service';
+import { Subscription } from 'rxjs';
+
 @Injectable({
   providedIn: 'root'
 })
 export class DataStorageService {
+  private url = "https://ng-complete-guid-d64c8-default-rtdb.asia-southeast1.firebasedatabase.app/";
+  recipeSubscription: Subscription;
 
-  constructor(private http: HttpClient) { }
+  constructor(private http: HttpClient, private recipesService: RecipeService) { }
+
+  storeRecipes() {
+    const recipes = this.recipesService.getRecipes();
+
+    this.recipeSubscription = this.http
+      .put(
+        this.url+'recipes.json', 
+        recipes
+      ).subscribe(
+        response => {
+          console.log(response);
+        }
+      );
+  }
 }
