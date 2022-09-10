ed97d5e52ba6dd3ac19f5eaa6edadab95406a1c7
Husni <service.has.solution@gmail.com>
2022-09-10 21:18:20 +0700

    291. Handling Form Input

--- src/app/auth/auth.component.ts
+++ src/app/auth/auth.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-auth',
@@ -7,13 +8,25 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AuthComponent implements OnInit {
   isLoginMode = false;
+  loginForm: FormGroup;
+  email = null;
+  password = null;
 
   constructor() { }
 
   ngOnInit(): void {
+    this.loginForm = new FormGroup({
+      'email': new FormControl(this.email, [Validators.required, Validators.email]),
+      'password': new FormControl(this.password, [Validators.required, Validators.minLength(6)])
+    })
   }
 
   onSwitchMode() {
     this.isLoginMode = !this.isLoginMode;
+  }
+
+  onSubmit() {
+    console.log(this.loginForm.value);
+    this.loginForm.reset();
   }
 }
