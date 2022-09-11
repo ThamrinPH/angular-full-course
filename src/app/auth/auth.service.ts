0ef7ea9a0ac886d0943b416b8f177b754d00b942
Husni <service.has.solution@gmail.com>
2022-09-11 21:01:27 +0700

    299. Login Error Handling

--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -1,4 +1,4 @@
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpErrorResponse } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { catchError } from 'rxjs/operators';
 import { throwError } from 'rxjs';
@@ -27,29 +27,9 @@ export class AuthService {
         returnSecureToken: true
         }
       )
-      .pipe(catchError(errorRes => {
-        let errorMessage = "An unknown error occured!";
-        if( !errorRes.error || !errorRes.error.error ) {
-          return throwError(errorMessage);
-        }
-
-        switch (errorRes.error.error.message) {
-          case 'EMAIL_EXISTS':
-            errorMessage = "The email address is already in use by another account.";
-            break;
-          case 'OPERATION_NOT_ALLOWED':
-            errorMessage = "Password sign-in is disabled for this project.";
-            break;
-          case 'TOO_MANY_ATTEMPTS_TRY_LATER':
-            errorMessage = "We have blocked all requests from this device due to unusual activity. Try again later.";
-            break;          
-          default:
-            errorMessage = errorRes.message;
-            break;
-        }
-
-        return throwError(errorMessage);
-      }))
+      .pipe(
+        catchError(this.handleError)
+      );
   }
 
   login(email: string, password: string) {
@@ -60,5 +40,33 @@ export class AuthService {
         returnSecureToken: true
       }
     )
+  }
+
+  private handleError( errorRes: HttpErrorResponse) {
+    let errorMessage = "An unknown error occured!";
+
+    if( !errorRes.error || !errorRes.error.error ) {
+      return throwError(errorMessage);
+    }
+
+    switch (errorRes.error.error.message) {
+      case 'EMAIL_EXISTS':
+        errorMessage = "The email address is already in use by another account.";
+        break;
+      case 'EMAIL_NOT_FOUND':
+        errorMessage = "This email does not exists.";
+        break;
+      case 'OPERATION_NOT_ALLOWED':
+        errorMessage = "Password sign-in is disabled for this project.";
+        break;
+      case 'TOO_MANY_ATTEMPTS_TRY_LATER':
+        errorMessage = "We have blocked all requests from this device due to unusual activity. Try again later.";
+        break;          
+      case 'INVALID_PASSWORD':
+        errorMessage = "This password is not correct.";
+        break;
+    }
+
+    return throwError(errorMessage);
   }
 }
