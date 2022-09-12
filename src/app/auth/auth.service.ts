f14b83564554b1d1795a55a957415d4c8afa3b99
Husni <service.has.solution@gmail.com>
2022-09-12 09:41:35 +0700

    305. Adding Auto-Login

--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -53,6 +53,31 @@ export class AuthService {
     )
   }
 
+  autoLogin() {
+    const userData: {
+      email: string,
+      id: string;
+      _token: string;
+      _tokenExpirationDate: string;
+    } = JSON.parse(localStorage.getItem('userData'));
+
+    if(!userData) {
+      return;
+    }
+
+    const loadedUser = new User(
+      userData.email, 
+      userData.id, 
+      userData._token, 
+      new Date(userData._tokenExpirationDate)
+    )
+
+    if(loadedUser.token) {
+      this.user.next(loadedUser);
+    }
+
+  }
+
   logout() {
     this.user.next(null);
     this.router.navigate(['/login']);
@@ -69,6 +94,7 @@ export class AuthService {
     );
 
     this.user.next(user);
+    localStorage.setItem('userData', JSON.stringify(user));
   }
 
   private handleError( errorRes: HttpErrorResponse) {
