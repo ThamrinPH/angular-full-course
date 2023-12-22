bd4e361c6420f31b4735a6a0d0528c22eec53315
Husni <service.has.solution@gmail.com>
2023-12-22 15:33:56 +0700

    init signals section

---
+++ src/app/signals/signals.component.ts
@@ -0,0 +1,24 @@
+import { NgFor } from '@angular/common';
+import { Component, signal } from '@angular/core';
+
+@Component({
+  selector: 'app-signals',
+  templateUrl: './signals.component.html',
+  standalone: true,
+  styleUrls: ['./signals.component.css'],
+  imports: [NgFor],
+})
+export class SignalsComponent {
+  actions: string[] = [];
+  counter = signal(0);
+
+  increment() {
+    this.counter++;
+    this.actions.push('INCREMENT');
+  }
+
+  decrement() {
+    this.counter--;
+    this.actions.push('DECREMENT');
+  }
+}
